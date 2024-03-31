67ba0874bc08844b01d8c5613b4b17f1532f015c
ArcherHeffern <heffernarcher@gmail.com>
2024-03-31 03:38:39 -0400

    some parsing

--- asm.py
+++ asm.py
@@ -1,7 +1,16 @@
 from sys import argv
+from typing import Iterable, NewType, Callable
 from dataclasses import dataclass
 from pathlib import Path
 from enum import Enum, auto
+
+"""
+TODO: 
+- ALlow for multiple register sets
+- Feature to add line numbers
+- MMU to translate addresses
+- History feature - go back in time
+"""
 
 EXT_SUCCESS = 0
 EXT_ERR_BAD_ARGUMENTS = 1
@@ -43,10 +52,8 @@ class TokenType(Enum):
 
     HALT = auto()
 
+    PRINT = auto()
 
-REGISTER_SET = (
-    "R1", "R2", "R3", "R4", "R5", "R6"
-)
 
 KEYWORDS = {
     "LOAD": TokenType.LOAD,
@@ -56,13 +63,62 @@ KEYWORDS = {
     "MUL": TokenType.MUL,
     "DIV": TokenType.DIV,
     "HALT": TokenType.HALT,
+    "PRINT": TokenType.PRINT,
 }
+
+
+@dataclass
+class Memory:
+    addresses: list[list[str]]
+    starting_address: int
+    parser: 'Parser'
+    scanner: 'Scanner'
+
+    registers = {
+        "R1": 0, 
+        "R2": 1, 
+        "R3": 2, 
+        "R4": 3, 
+        "R5": 4, 
+        "R6": 5,
+    }
+
+    status_registers = {
+        "ip": 0
+    }
+
+    def __get_instruction(self):
+        return self.addresses[self.__mmu(self.status_registers["ip"])]
+
+    def fde_cycle(self):
+        while (True):
+            instruction = self.__get_instruction()
+            tokens = self.scanner.scan_line(instruction)
+            ast = self.parser(tokens)
+            ast()
+            self.status_registers["ip"] += 1
+
+    def get_register(self, register: str):
+        return self.REGISTERS[register]
+
+    def set_register(self, register: str, value: int):
+        self.REGISTERS[register] = value
+
+    def __mmu(self, address):
+        return address - self.starting_address
+
+    def get_val(self, address: int):
+        return self.addresses[self.__mmu(address)]
+
+    def set_val(self, address: int, value: str):
+        self.addresses[self.__mmu(address)] = value
 
 @dataclass
 class Token:
     tokentype: TokenType
     lexeme: str
     literal: str|None 
+    line_number: int
 
     def __repr__(self):
         if self.literal is not None:
@@ -72,38 +128,16 @@ class Token:
 class Scanner:
     def __init__(self, lines: list[str]):
         self.lines = lines
-        self.line_number = 1
 
-        self.line = None
+    
+    def scan_line(self, line_number: int, memory: Memory) -> list[Token]|None:
+        # Actual Line Number no MMU
         self.start = 0
         self.curr = 0
-        self.tokens: list[Token] = []
+        self.line_number = line_number
+        self.line = memory.get_val(line_number)
+        self.tokens = []
 
-    def scan(self) -> list[list[Token]]:
-        lines: list[list[Token]] = []
-        for line in self.lines:
-            self.line = line
-            self.scan_line()
-            lines.append(self.tokens)
-            self.start = 0
-            self.curr = 0
-            self.line_number += 1
-            self.tokens = []
-        return lines
-
-    def __is_at_end(self):
-        return self.curr >= len(self.line)
-
-    def __advance(self):
-        c = self.line[self.curr]
-        self.curr += 1
-        return c
-    
-    def __add_token(self, token: TokenType, literal: str|None = None):
-        lex = Token(token, self.line[self.start: self.curr], literal)
-        self.tokens.append(lex)
-    
-    def scan_line(self) -> list[Token]|None:
         while not self.__is_at_end():
             self.start = self.curr
             c = self.__advance()
@@ -126,9 +160,22 @@ class Scanner:
                     if c.isnumeric():
                         self.__number()
                     elif c.isalpha():
-                        self.__identifier()
+                        self.__identifier(memory)
                     else: 
                         error("Lexeme not found")
+        return self.tokens
+
+    def __is_at_end(self):
+        return self.curr >= len(self.line)
+
+    def __advance(self):
+        c = self.line[self.curr]
+        self.curr += 1
+        return c
+    
+    def __add_token(self, token: TokenType, literal: str|None = None):
+        lex = Token(token, self.line[self.start: self.curr], literal, self.line_number)
+        self.tokens.append(lex)
 
     def __number(self):
         while self.__peek().isnumeric():
@@ -137,7 +184,7 @@ class Scanner:
         self.__add_token(TokenType.NUMBER, number)
         ...
 
-    def __identifier(self):
+    def __identifier(self, memory: Memory):
         while self.__peek().isalnum():
             self.__advance()
         val = self.line[self.start: self.curr]
@@ -145,17 +192,154 @@ class Scanner:
             self.__advance()
             self.__add_token(TokenType.LABEL, val)
         else:
-            self.__add_token(TokenType.LITERAL, val)
+            token_type = None
+            if val in KEYWORDS:
+                token_type = KEYWORDS[val]
+            elif val in memory.registers:
+                token_type = TokenType.REGISTER
+            else:
+                token_type = TokenType.LITERAL
+            self.__add_token(token_type, val)
 
     def __peek(self) -> str:
         if self.__is_at_end():
             return "\0"
         return self.line[self.curr]
+
+class Parser:
+    def __init__(self, memory: Memory):
+        self.memory = memory
     
-    def __peek_next(self) -> str:
-        if self.curr + 1 >= len(self.line):
+    def __parse_statement(self, tokens) -> Callable:
+        self.tokens = tokens
+        self.current = 0
+
+        curr = self.__peek()
+        match curr.tokentype:
+            case TokenType.LOAD:
+                statement = self.__parse_load_statement()
+            case TokenType.STORE:
+                statement = self.__parse_store_statement()
+            case TokenType.ADD:
+                statement = self.__parse_add_statement()
+            case TokenType.SUB:
+                statement = self.__parse_sub_statement()
+            case TokenType.MUL:
+                statement = self.__parse_mul_statement()
+            case TokenType.DIV:
+                statement = self.__parse_div_statement()
+            case TokenType.HALT:
+                statement = self.__parse_halt_statement()
+            case TokenType.PRINT:
+                statement = self.__parse_print_statement()
+            case _:
+                raise Exception(f"Token {curr} handled while scanning")
+        return statement
+
+    def __parse_load_statement(self):
+        self.__match(TokenType.LOAD)
+        self.__match(TokenType.REGISTER)
+        register = self.__previous()
+        self.__match(TokenType.COMMA)
+        value_at = self.__parse_load_addr_types()
+        def lambda_():
+            self.memory.registers[register.lexeme] = value_at
+        return lambda_
+        ...
+    
+    def __parse_load_addr_types(self):
+        # TODO: How to handle if there are many possibilities: Eg. All the addressing types
+        # Returns a function returning the actual value - will need to calculate on the fly
+        # This one doesn't call the function because its more of a logic thingy
+        curr = self.__peek()
+        match curr:
+            case TokenType.NUMBER:
+                func = self.__parse_direct_addr()
+            case TokenType.EQUALS:
+                func = self.__parse_immediate_addr()
+            case TokenType.LBRACET:
+                func = self.__parse_index_addr()
+            case TokenType.AT:
+                func = self.__parse_index_addr()
+            case TokenType.DOLLAR:
+                func = self.__parse_relative_addr()
+            case _:
+                raise Exception("Error parsing load addr")
+        return func
+        ...
+
+    def __parse_store_statement(self):
+        ...
+    def __parse_add_statement(self):
+        ...
+    def __parse_sub_statement(self):
+        ...
+    def __parse_mul_statement(self):
+        ...
+    def __parse_div_statement(self):
+        ...
+    def __parse_halt_statement(self):
+        ...
+    def __parse_print_statement(self):
+        ...
+
+
+    def __parse_direct_addr(self):
+        number = self.__match(TokenType.NUMBER).literal
+        def lambda_():
+            return self.memory.get_val(number)
+        return lambda_
+    
+    def __parse_immediate_addr(self):
+        self.__match(TokenType.EQUALS)
+        number = self.__match(TokenType.NUMBER).literal
+        def lambda_():
+            return number
+        return lambda_
+
+    def __parse_index_addr(self):
+        self.__match(TokenType.LBRACET)
+        if not (num := self.__match(TokenType.NUMBER)):
+            raise Exception("Index addressing expected number")
+        if not self.__match(TokenType.COMMA):
+            raise Exception("Index addressing expected comma")
+        if not (register := self.__match(TokenType.REGISTER)):
+            raise Exception("Index addressing expected register")
+        def lambda_():
+            return self.memory[num + self.memory.registers[register.lexeme]]
+        return lambda_
+
+    def __parse_indirect_addr(self):
+        ...
+
+    def __parse_relative_addr(self):
+        ...
+
+    
+    def __previous(self) -> Token:
+        return self.tokens[self.current - 1]
+
+    def __advance(self) -> Token:
+        self.current += 1
+        return self.__previous()        
+
+    def __peek(self) -> TokenType:
+        if self.__is_at_end():
             return "\0"
-        return self.line[self.curr + 1]
+        return self.tokens[self.current]
+    
+    def __check(self, token: TokenType) -> bool:
+        return self.__peek() == token
+    
+    def __match(self, tokens: Iterable[TokenType]) -> Token|None:
+        for token in tokens:
+            if self.__check(token):
+                return self.__advance()
+        return None
+
+
+    def __is_at_end(self):
+        return self.current >= len(self.tokens)
         
 
 if len(argv) != 2:
@@ -170,6 +354,21 @@ if not filename.is_file():
 
 with open(filename, "r") as f:
     lines = f.readlines()
-    scanner = Scanner(lines)
-    tokens = scanner.scan()
-    print(tokens)
+    tokens = Scanner(lines).scan()
+    ast = Parser(tokens).parse()
+
+
+    # Virtual Machine
+    registers = [0] * len(REGISTERS)
+    ip = 0
+    memory_address = 0
+    for token_list in tokens:
+        if len(token_list) == 0:
+            continue
+        match token_list[0]:
+            case TokenType.LOAD:
+                
+
+
+
+
